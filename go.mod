f226107b3f30144da33b1e29bbf409f618044e59
Ray Ozzie <ray@ozzie.net>
2022-08-15 14:32:42 -0400

    m

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/blues/archive
 
 go 1.17
+
+require github.com/blues/note-go v1.5.0 // indirect
--- http-root.go
+++ http-root.go
@@ -5,30 +5,112 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
+	"strconv"
+	"strings"
+
+	"github.com/blues/note-go/note"
 )
 
 // Root handler
-func inboundWebRootHandler(httpRsp http.ResponseWriter, httpReq *http.Request) {
+func inboundWebRootHandler(w http.ResponseWriter, r *http.Request) {
 
-	// Process the request URI, looking for things that will indicate "dev"
-	method := httpReq.Method
-	if method == "" {
-		method = "GET"
-	}
-
-	// Get the target
-	parsedURL, _ := url.Parse(httpReq.RequestURI)
+	// Get parameters from the request header, validating as we go along
+	parsedURL, _ := url.Parse(r.RequestURI)
 	target := path.Base(parsedURL.Path)
-
-	// Exit if just the favicon
 	if target == "favicon.ico" {
 		return
 	}
 
-	// Done
-	httpRsp.Write([]byte("I'm watching you."))
+	eventJSON, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		writeErr(w, err.Error())
+		return
+	}
+	if len(eventJSON) == 0 {
+		writeErr(w, "event is blank")
+		return
+	}
+	var event note.Event
+	err = note.JSONUnmarshal(eventJSON, &event)
+	if err != nil {
+		writeErr(w, err.Error())
+		return
+	}
 
+	archiveID, exists := headerField(r, "archive_id")
+	if !exists {
+		writeErr(w, "archive_id not specified")
+		return
+	}
+	s, _ := headerField(r, "archive_mins")
+	archiveMins, _ := strconv.Atoi(s)
+	if archiveMins <= 0 {
+		archiveMins = 1440
+	}
+
+	bucketEndpoint, _ := headerField(r, "bucket_endpoint")
+
+	bucketName, exists := headerField(r, "bucket_name")
+	if !exists {
+		writeErr(w, "bucket_name not specified")
+		return
+	}
+
+	bucketRegion, exists := headerField(r, "bucket_region")
+	if !exists {
+		writeErr(w, "bucket_region not specified")
+		return
+	}
+
+	fileAccess, exists := headerField(r, "file_access")
+	if !exists {
+		writeErr(w, "file_access not specified")
+		return
+	}
+
+	fileFormat, exists := headerField(r, "file_format")
+	if !exists {
+		fileFormat = "[id]/[year]-[month]/[device]/[when]"
+	}
+
+	fileName, exists := headerField(r, "file_name")
+	if !exists {
+		writeErr(w, "file_name not specified")
+		return
+	}
+
+	keyID, exists := headerField(r, "key_id")
+	if !exists {
+		writeErr(w, "key_id not specified")
+		return
+	}
+	keySecret, exists := headerField(r, "key_secret")
+	if !exists {
+		writeErr(w, "key_secret not specified")
+		return
+	}
+
+	// Debug
+	fmt.Printf("archiveID:%s archiveMins:%d bucketEndpoint:%s bucketName:%s bucketRegion:%s fileAccess:%s fileFormat:%s fileName:%s keyID:%s keySecret:%s\n%s\n\n", archiveID, archiveMins, bucketEndpoint, bucketName, bucketRegion, fileAccess, fileFormat, fileName, keyID, keySecret, string(eventJSON))
+
+	// Done
+	w.Write([]byte("I'm watching you."))
+
+}
+
+// Clean comments out of the specified field
+func headerField(r *http.Request, fieldName string) (out string, exists bool) {
+	s1 := r.Header.Get(fieldName)
+	s2 := strings.TrimSpace(strings.Split(s1, " ")[0])
+	return s2, s2 != ""
+}
+
+// Write an error message as a JSON object
+func writeErr(w http.ResponseWriter, message string) {
+	w.Write([]byte(fmt.Sprintf("{\"err\":\"%s\"}", message)))
 }
